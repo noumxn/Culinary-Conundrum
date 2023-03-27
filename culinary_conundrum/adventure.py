eee51238a4ca6c8943fdd167b78fd33d39240589
nouman <syednouman2114@gmail.com>
2023-03-27 02:17:08 -0400

    add new extensions

--- culinary_conundrum/adventure.py
+++ culinary_conundrum/adventure.py
@@ -4,26 +4,53 @@ import sys
 with open(sys.argv[1]) as f:
     game_map = json.load(f)
 
+print("  ______      _ _                             ______                            _                   ")
+print(" / _____)    | (_)                           / _____)                          | |                  ")
+print("| /     _   _| |_ ____   ____  ____ _   _   | /      ___  ____  _   _ ____   _ | | ____ _   _ ____  ")
+print("| |    | | | | | |  _ \ / _  |/ ___) | | |  | |     / _ \|  _ \| | | |  _ \ / || |/ ___) | | |    \ ")
+print("| \____| |_| | | | | | ( ( | | |   | |_| |  | \____| |_| | | | | |_| | | | ( (_| | |   | |_| | | | |")
+print(" \______)____|_|_|_| |_|\_||_|_|    \__  |   \______)___/|_| |_|\____|_| |_|\____|_|    \____|_|_|_|")
+print("                                   (____/                                                           ")
+print("\n\033[1mWelcome to the game!\033[0m\n")
+
+
 # Setting the starting location
 current_location = game_map[0]
 
 # Player inventory
 inventory = []
 
+# Player money
+money = 20
+
 # Valid commands
-command_list = ["go", "get", "look", "inventory", "quit", "help", "drop"]
+command_list = ["go", "get", "look", "inventory",
+                "quit", "help", "drop", "assist", "prepare"]
+
+prices = {
+    "milk": 4,
+    "salt": 1,
+    "sugar": 2,
+    "vanilla essence": 4,
+    "baking powder": 2,
+    "flour": 2,
+    "butter": 3
+}
 
 while True:
-    print("> " + current_location["name"])
-    print()
-    print(current_location["desc"])
-    print()
+    print("> \033[1m" + current_location["name"] + "\033[0m\n")
+    if current_location["name"] == "John's Front Yard" and current_location["flag"] == "false":
+        print("\033[3m" + current_location["desc2"] + "\033[0m\n")
+    else:
+        print("\033[3m" + current_location["desc"] + "\033[0m\n")
     if "items" in current_location:
-        print("Items:", ", ".join(current_location["items"]))
-        print()
+        print("Items:", ", ".join(current_location["items"]) + "\n")
 
     print("Directions:", " ".join(current_location["direction"].keys()))
     print()
+
+    if "purse" in inventory:
+        print("Money: $" + str(money) + "\n")
 
     command = input("What would you like to do? ").strip().lower()
 
@@ -58,9 +85,15 @@ while True:
 
     elif command.startswith("get "):
         item_name = command[4:]
+        if item_name in prices and "purse" not in inventory:
+            print("Uh oh! You can't pick this item without paying for it.\n")
+            continue
+
         if "items" in current_location and item_name in current_location["items"]:
             inventory.append(item_name)
             current_location["items"].remove(item_name)
+            if item_name in prices:
+                money = money - prices[item_name]
             print("You pick up the", item_name + ".\n")
         else:
             print("There's no", item_name, "here.\n")
@@ -74,6 +107,12 @@ while True:
         else:
             print("There's no", item_name, "in your inventory.\n")
 
+    elif command == "assist":
+        print("Awesome job! You've helped John get his hen back in the coop.")
+        print("John gives you some eggs as a token on his appriciation.\n")
+        inventory.append("eggs")
+        current_location["flag"] = "false"
+
     elif command == "help":
         print("You can run the following commands:")
         for i in command_list:
@@ -83,3 +122,17 @@ while True:
         if command == "quit":
             print("Goodbye!")
             sys.exit()
+        if command == "assist":
+            print("Awesome job! You've helped John get his hen back in the coop.")
+            print("John gives you some eggs as a token on his appriciation.\n")
+            inventory.append("eggs")
+            current_location["flag"] = "false"
+
+    elif command == "prepare":
+        if current_location == game_map[0]:
+            if ["eggs", "milk", "butter", "flour", "baking powder", "salt", "sugar", "vanilla essence"] not in current_location["items"]:
+                print("Left")
+            else:
+                print("Done")
+        else:
+            print("You need to be in the Kitchen to prepare your ingredients.\n")
